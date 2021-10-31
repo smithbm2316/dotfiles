ab6abac33780f4202088f45c28d7f4ae0ec7acac
Ben Smith <bsmithdev@mailbox.org>
2021-10-30 19:32:27 -0700

    started work on fish shell ssh updates

--- fish/config.fish
+++ fish/config.fish
@@ -26,11 +26,9 @@ end
 # OBS virtual cam using v4l2loopback 
 abbr -a vcam 'sudo modprobe v4l2loopback video_nr=7 card_label="OBS Virtual Cam"'
 # Source fish
-abbr -a sosh 'source ~/.config/fish/config.fish'
+abbr -a sosh 'source ~/dotfiles/fish/config.fish'
 # shortcut to get to packer plugins directory
 abbr -a nvpack 'cd ~/.local/share/nvim/site/pack/packer/start'
-# browser-sync
-abbr -a bsync "browser-sync start --server --files '*.css, *.html, *.js' --no-open"
 # `cd -` alias
 abbr -a -- - 'cd -'
 # lazygit
@@ -48,11 +46,16 @@ abbr -a yeet 'rm -rf'
 # stow helper for dotfiles and cronjobs
 alias cf "$HOME/dotfiles/scripts/cf.sh"
 alias stowdots "$HOME/dotfiles/scripts/stowdots.fish"
-# exa aliases
-alias l 'exa --icons -a'
-alias ls 'exa --icons'
-alias ll 'exa --icons -l'
-alias lla 'exa --icons -la'
+# ls/exa aliases
+if command -v exa &>/dev/null
+  alias l 'exa --icons -a'
+  alias ls 'exa --icons'
+  alias ll 'exa --icons -l'
+  alias tree 'exa --icons --tree --all'
+else
+  alias l 'ls -lA'
+  alias ll 'ls -l'
+end
 # .. commands
 abbr -a ... ../../
 abbr -a .... ../../../
@@ -61,6 +64,9 @@ abbr -a ...... ../../../../../
 abbr -a ....... ../../../../../../
 abbr -a ........ ../../../../../../../
 abbr -a ......... ../../../../../../../../
+# nvim
+abbr -a nv 'nvim'
+abbr -a nvs '/usr/bin/nvim'
 
 
 
@@ -76,51 +82,18 @@ function c
 end
 function dots
   set -l fileloc (fd -t f --color=never . $HOME/dotfiles | fzf --preview='head -80 {}')
-  nvim $fileloc -c 'cd ~/dotfiles'
+  $EDITOR $fileloc -c 'cd ~/dotfiles'
 end
 function f
   set -l file_to_open (fzf --preview='head -80 {}')
   if test -f $PWD/$file_to_open
-    nvim $file_to_open
+    $EDITOR $file_to_open
   end
 end
-
-# tree
-function tree
-  switch $argv
-    case '-a'
-      exa --icons --tree -all
-    case '*'
-      exa --icons --tree --all -I "node_modules|.git" $argv
-  end
-end
-
-# nvim
-function nv
-  set -l nvstart 'nvim ~/.config/nvim/lua/my'
-  set -l nvend '-c "cd ~/.config/nvim"'
-
-  switch $argv
-    case 'plug'
-      eval "$nvstart/plugins.lua $nvend"
-    case 'plugs'
-      eval "$nvstart/plugs/init.lua $nvend"
-    case 'maps'
-      eval "$nvstart/maps.lua $nvend"
-    case 'sets'
-      eval "$nvstart/settings.lua $nvend"
-    case 's'
-      nvim -c 'RestoreSession<cr>'
-    case '*'
-      nvim $argv
-  end
-end
-
-abbr -a nvs '/usr/bin/nvim'
 
 # shortcuts for shell configs
 function shl
-  set -l shlcmd 'nvim ~/dotfiles/fish'
+  set -l shlcmd "$EDITOR ~/dotfiles/fish"
 
   switch $argv
     case 'rc'
@@ -130,7 +103,7 @@ function shl
     case 'hist'
       eval "$shlcmd/history"
     case 'fns'
-      nvim "$shlcmd/functions"
+      eval "$shlcmd/functions"
     case ''
       fish
     case '*'
@@ -159,9 +132,13 @@ abbr -a tma 'tmux attach -t '
 
 function tm -a session
   if test -z $session
-    tmux attach
+    tmux attach 2>/dev/null
   else
-    tmux attach -t $session
+    tmux attach -t $session 2>/dev/null
+  end
+  
+  if test $status -ne 0
+    tmux
   end
 end
 
@@ -175,10 +152,9 @@ end
 
 function tmn -a session
   if test -z $session
-    # tmux new -s (basename $PWD)
     tmux \
       new -n code -s (basename $PWD) \; \
-      send 'nvim' C-m \; \
+      send "$EDITOR" C-m \; \
       neww -n serve \; \
       splitw -h -c $idk -t serve
   else
@@ -189,7 +165,7 @@ end
 function tmnp -a session serve_command
   tmux \
     new -n code -s (basename $PWD) \; \
-    send 'nvim' C-m \; \
+    send "$EDITOR" C-m \; \
     neww -n serve \; \
     splitw -h -c $idk -t serve \; \
     send -c $serve_command C-m
@@ -215,30 +191,6 @@ function g
       git $argv
   end
 end
-
-# count lines of code, defaults to looking for web dev files
-function loc
-  if test (count $argv) = 0
-    rg --files -I 'node_modules|.git' **.{ts,tsx,js,jsx,css,html} 2>/dev/null | xargs cat | wc -l | xargs
-  else
-    rg --files -I 'node_modules|.git' $argv 2>/dev/null | xargs cat | wc -l | xargs
-  end
-end
-
-# jira alias
-# function jira
-#   set -l jiraCmd "$GOBIN/jira"
-#   if test (count $argv) -lt 2
-#     switch $argv
-#       case 'lsme'
-#         eval "$jiraCmd ls -a 'Ben Smith'"
-#       case '*'
-#         eval "$jiraCmd $argv"
-#     end
-#   else
-#     eval "$jiraCmd $argv"
-#   end
-# end
 
 
 
@@ -283,11 +235,12 @@ set fish_cursor_visual block
 # os-specific settings
 #
 ##################################################
+
 # Linux settings
 if test (uname -s) = 'Linux'
   # setup keychain settings if not in tmux
   if test -z $TMUX && status --is-interactive
-    SHELL=/usr/bin/fish keychain --eval --quiet -Q id_ed25519_gh | source
+    # SHELL=/usr/bin/fish keychain --eval --quiet -Q id_ed25519_gh | source
   end
 
   # Debian settings
@@ -314,3 +267,23 @@ else if test (uname -s) = 'Darwin'
 	set -p PATH /usr/local/opt/gnu-sed/libexec/gnubin
 end
 fish_add_path /usr/local/sbin
+
+
+
+##################################################
+#
+# load ssh keys
+#
+##################################################
+function load_ssh_keys
+  set -l fish_cmd (command -v fish)
+  set -l ls_cmd (command -v ls)
+  set -l keychain_cmd (command -v keychain)
+  set -l ssh_keys ($ls_cmd ~/.ssh | grep -qv -e pub -e known_hosts -e config)
+  echo "SHELL=$fish_cmd $keychain_cmd --agents ssh --eval --quiet -Q $ssh_keys | source"
+  SHELL=$fish_cmd $keychain_cmd --eval --quiet -Q $ssh_keys | source
+end
+
+if test -z (pgrep ssh-agent | string collect)
+  eval (ssh-agent -c)
+end
