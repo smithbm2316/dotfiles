bcd89d8e4c965b189c916c026dae0e5bd6434f37
Ben Smith <bsmithdev@mailbox.org>
2024-01-23 16:30:22 -0800

    improve watch-video.sh script to allow watching playlists

--- scripts/watch-video.sh
+++ scripts/watch-video.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 link="$(xclip -sel clip -o)"
 
-# check if the link is a youtube link, if so transform it properly from a potential playlist link
+# validate that we passed a youtube link to the program first, exit early if we didn't
 if echo "$link" | grep 'youtube.com'; then
-  link="$(echo "$link" | sed -nr 's/^(.*?v=[A-Za-z0-9\-]+)&?.*/\1/p')"
+  # check if this is a valid watch later youtube link, if so transform it to a normal video link without the playlist details attached
+  if echo "$link" | grep 'list=WL'; then
+    link="https://youtube.com/watch?v=$(echo "$link" | sed -nr 's/^.*[?&]v=([^?&]+?)[?&].*$/\1/p')"
+    notify-send 'Opening mpv with youtube video from watch later...' "$link"
+  else
+    notify-send 'Opening mpv with youtube video...' "$link"
+  fi
+else
+  notify-send 'Link was not a valid youtube video' "$link"
+  exit 1
 fi
 
-notify-send 'Opening mpv with video from clipboard...' "$link"
 mpv --title='mpv' "$link"
