1086300814087d01dda7af2472198dbf33e1f193
Ben Smith <bsmithdev@mailbox.org>
2024-07-09 17:58:48 -0700

    feat(nvim): add global insert_at_cursor_map helper

--- nvim/after/ftplugin/embedded_template.lua
+++ nvim/after/ftplugin/embedded_template.lua
@@ -10,18 +10,8 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
     { clear = true }
   ),
   callback = function()
-    -- insert ego template while already in insert mode (go shortcut)
-    vim.keymap.set('i', '<c-i>t', function()
-      -- get the current line and cursor position
-      local line = vim.api.nvim_get_current_line()
-      local cursor = vim.api.nvim_win_get_cursor(0)
-      -- insert new template at the current cursor position in our line
-      line = line:sub(1, cursor[2]) .. '<% %>' .. line:sub(cursor[2] + 1)
-      -- prepare the cursor to be 2 characters more before we update the line contents
-      cursor[2] = cursor[2] + 2
-      -- update the line and cursor position
-      vim.api.nvim_set_current_line(line)
-      vim.api.nvim_win_set_cursor(0, cursor)
-    end, { desc = 'Insert embedded template tag' })
+    insert_at_cursor_map('<c-i>t', '<% %>', 'template')
+    insert_at_cursor_map('<c-i>.', '<% %>', 'template')
+    insert_at_cursor_map('<c-i>=', '<%= %>', 'template')
   end,
 })
--- nvim/after/ftplugin/php.lua
+++ nvim/after/ftplugin/php.lua
@@ -2,43 +2,16 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
   pattern = { '*.blade.php', '*.php' },
   group = vim.api.nvim_create_augroup('PhpFiletypeCmds', { clear = true }),
   callback = function()
-    vim.keymap.set('i', '<c-i>-', function()
-      -- get the current line and cursor position
-      local line = vim.api.nvim_get_current_line()
-      local cursor = vim.api.nvim_win_get_cursor(0)
-      -- insert '->' at the current cursor position in our line
-      line = line:sub(1, cursor[2]) .. '->' .. line:sub(cursor[2] + 1)
-      -- prepare the cursor to be 2 characters more before we update the line contents
-      cursor[2] = cursor[2] + 2
-      -- update the line and cursor position
-      vim.api.nvim_set_current_line(line)
-      vim.api.nvim_win_set_cursor(0, cursor)
-    end, { desc = 'Insert "->" (php shortcut)' })
+    insert_at_cursor_map('<c-i>-', '->', 'php')
+    insert_at_cursor_map('<c-i>.', '->', 'php')
 
-    vim.keymap.set('i', '<c-i>;', function()
-      -- get the current line and cursor position
-      local line = vim.api.nvim_get_current_line()
-      local cursor = vim.api.nvim_win_get_cursor(0)
-      -- insert '->' at the current cursor position in our line
-      line = line:sub(1, cursor[2]) .. '::' .. line:sub(cursor[2] + 1)
-      -- prepare the cursor to be 2 characters more before we update the line contents
-      cursor[2] = cursor[2] + 2
-      -- update the line and cursor position
-      vim.api.nvim_set_current_line(line)
-      vim.api.nvim_win_set_cursor(0, cursor)
-    end, { desc = 'Insert "::" (php shortcut)' })
+    insert_at_cursor_map('<c-i>;', '::', 'php')
+    insert_at_cursor_map('<c-i>c', '::class', 'php')
 
-    vim.keymap.set('i', '<c-i>=', function()
-      -- get the current line and cursor position
-      local line = vim.api.nvim_get_current_line()
-      local cursor = vim.api.nvim_win_get_cursor(0)
-      -- insert '->' at the current cursor position in our line
-      line = line:sub(1, cursor[2]) .. '=>' .. line:sub(cursor[2] + 1)
-      -- prepare the cursor to be 2 characters more before we update the line contents
-      cursor[2] = cursor[2] + 2
-      -- update the line and cursor position
-      vim.api.nvim_set_current_line(line)
-      vim.api.nvim_win_set_cursor(0, cursor)
-    end, { desc = 'Insert "=>" (php shortcut)' })
+    insert_at_cursor_map('<c-i>=', '=>', 'php')
+
+    insert_at_cursor_map('<c-i>v', '$', 'php')
+
+    insert_at_cursor_map('<c-i>t', '$this->', 'php')
   end,
 })
