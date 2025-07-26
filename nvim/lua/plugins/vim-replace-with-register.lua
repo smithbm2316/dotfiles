1079eeb5e2ab361ce721d2ee6ebef7df6e6b1478
Ben Smith <ben@bensmith.sh>
2025-07-26 09:30:34 -0700

    nvim: fix ReplaceWithRegister keybinds

--- nvim/lua/plugins/vim-replace-with-register.lua
+++ nvim/lua/plugins/vim-replace-with-register.lua
@@ -1,4 +1,26 @@
 return {
   'vim-scripts/ReplaceWithRegister',
   event = 'VeryLazy',
+  config = function()
+    -- need this to override the new default neovim keybinding `gr` for lsp-related
+    -- actions when a new lsp server attaches to a buffer
+    -- issue that discovered this for me: https://github.com/LazyVim/LazyVim/discussions/1371
+    vim.keymap.del({ 'n', 'v' }, 'gra')
+    vim.keymap.del({ 'n' }, 'gri')
+    vim.keymap.del({ 'n' }, 'grn')
+    -- vim.keymap.del({ 'n' }, 'g0')
+
+    vim.keymap.set({ 'n', 'o' }, 'gr', '<Plug>ReplaceWithRegisterOperator', {
+      desc = 'ReplaceWithRegisterOperator',
+      remap = true,
+    })
+    vim.keymap.set({ 'v' }, 'gr', '<Plug>ReplaceWithRegisterVisual', {
+      desc = 'ReplaceWithRegisterVisual',
+      remap = true,
+    })
+    vim.keymap.set({ 'n', 'o' }, 'grr', '<Plug>ReplaceWithRegisterLine', {
+      desc = 'ReplaceWithRegisterLine',
+      remap = true,
+    })
+  end,
 }
