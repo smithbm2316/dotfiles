700b390e9a1974d63cef9db18d8b1bd59f3105f3
Ben Smith <ben@bensmith.sh>
2026-01-24 15:14:45 -0800

    nvim: updates mini.notify notifications to appear in top right corner on smaller screens

--- nvim/after/plugin/mini.notify.lua
+++ nvim/after/plugin/mini.notify.lua
@@ -1,10 +1,18 @@
+local columns = vim.api.nvim_get_option_value('columns', {})
+local anchor = 'NW'
+local col = 0
+if columns <= 140 then
+  anchor = 'NE'
+  col = columns
+end
+
 require('mini.notify').setup {
   window = {
     ---@type vim.api.keyset.win_config
     config = {
-      anchor = 'NW',
+      anchor = anchor,
       row = 0,
-      col = 0,
+      col = col,
     },
   },
 }
